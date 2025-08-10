e4a305663c75c2ef4b52dd9843a566df84391e73
YusufByn <yusuf.buyukaydin33@gmail.com>
2025-08-10 17:37:28 +0200

    fifth commit session debugage fini, tout fonctionne au niveau du create et read

--- index.php
+++ index.php
@@ -5,16 +5,25 @@
 // pourquoi Once ? (une fois), c'est pour éviter le fait de recharger la base de données et donc créer des bugs
 require_once "config/add_task.php";
 
+// création d'un tableau vide qu'on appelle errors pour les futurs erreur qui peuvent occurer sur le site
+// vide car il aura plusieurs messages différents, ca me permet d'éviter de créer plusieurs variable diff de msg erreur
 $errors = [];
+
+// je déclare une variable $pdo (qui est dans add_task mais pas ici) et je lui rappelle la fonction linkDatabase
+// me permet donc d'établir la connexion à ma base de données
+$pdo = linkDatabase();
+
+
 // 1 ) Create du CRUD, traitement des données etc
 // condition pour vérifier si on a recu une request en post (formulaire)
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $formTitle = htmlspecialchars(trim($_POST["title"] ?? ""));
     $formDescription = htmlspecialchars(trim($_POST["description"] ?? ""));
-    $formStatus = htmlspecialchars(trim($_POST["status"] ?? ""));
+    $formStatus = htmlspecialchars(trim($_POST["status_test"] ?? ""));
     $formPriority = htmlspecialchars(trim($_POST["priority"] ?? ""));
-    $formDate = trim($_POST["due_at"] ?? "");
+    $formDate = trim($_POST["due_date"] ?? "");
 
+    var_dump($formStatus); 
     // validation du titre
     if (empty($formTitle)) {
         $errors[] = "Le titre est obligatoire.";
@@ -23,30 +32,30 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (empty($formDescription)) {
         $errors[] = "La description est obligatoire.";
     }
-
-    $formDate = trim($_POST['due_at'] ?? '');
-
-    if ($formDate !== '') {
-    $date = DateTime::createFromFormat('Y-m-d', $formDate);
-    if (!$date || $date->format('Y-m-d') !== $formDate) {
-        // date invalide
-        $errors[] = "La date n'est pas valide.";
-        }
+    // validation du statut
+    if (empty($formStatus)) {
+        $errors[] = "Le statut est obligatoire";
+    }
+    // validation de la prioritée
+    if (empty($formPriority)) {
+        $errors[] = "La saisie de prioritée est obligatoire";
+    }
+    // validation de la date
+    if (empty($formDate)) {
+        $errors[] = "La date est obligatoire";
     }
 
+
     if (empty($errors)) {
-        $stmt = $pdo->prepare("INSERT INTO tasks (title, description, priority, due_at, created_at, updated_at) 
-        VALUES (?, ?, ?, ?, NOW(), NOW())");
+        $insertTaks = $pdo->prepare("INSERT INTO tasks (title, description, status, priority, due_date, created_at, updated_at) 
+        VALUES (?, ?, ?, ?, ?, NOW(), NOW())");
         // les deux derniers champs created_at et updated_at sont automatiquement remplis par la fonction SQL NOW()
-        $stmt->execute([$formTitle, $formDescription, $formPriority, $formDueAt]);
-        // eventuellement redirection pour éviter le repost (header('Location: ...'))
+        $insertTaks->execute([$formTitle, $formDescription, $formStatus, $formPriority, $formDate]);
     }
 
 }
 
 // 2) LE READ (SELECT) POUR RECUPERER ET AFFICHER MES DONNEES:
-// je déclare une variable $pdo (qui est dans add_task mais pas ici) et je lui rappelle la fonction linkDatabase
-$pdo = linkDatabase();
 
 // je déclare ma variable tableElements, qui contient la variable pdo 
 // qui fait une requete pour aller chercher TOUT depuis le tableau task
@@ -79,26 +88,42 @@ $tasks = $tableElements->fetchAll();
     j'utilise include pour inclure cette page la ici  -->
     <?php include "headerandfooter/header.php";?>
     <main>
+        <!-- création d'une class formContainer pour mon formulaire -->
         <section class="formContainer">
             <form method="POST">
-                <input type="text" name="title" required>
-                <textarea name="description" required></textarea>
+                <label for="">Titre</label>
+                <input type="text" name="title" placeholder="Entrez votre titre">
 
-                <select name="status">
+                <label for="">Description</label>
+                <textarea name="description" placeholder="Entrez une description"></textarea>
+                
+                <!-- select en html me permet de faire une liste de selection
+                 les options sont donc les options à selectionner dans cette liste
+                 sauf disabled selected ce qui veut dire que tu peux pas le selectionner il est desactivé -->
+
+                <!-- j'ai mis les value des options en francais car sur mySQL elles sont en francais
+                 j'ai verifié avec cette requete : SHOW COLUMNS FROM tasks LIKE 'status'; -->
+                <label for="">Statut de la tâche</label>
+                <select name="status_test">
                     <option value="" disabled selected>Status</option>
-                    <option value="pending">À faire</option>
-                    <option value="in_progress">En cours</option>
-                    <option value="done">Terminée</option>
+                    <option value="à faire">À faire</option>
+                    <option value="en cours">En cours</option>
+                    <option value="terminée">Terminée</option>
                 </select>
 
+
+                <!-- j'ai mis les value des options en francais car sur mySQL elles sont en francais
+                 j'ai verifié avec cette requete : SHOW COLUMNS FROM tasks LIKE 'priority'; -->
+                <label for="">Prioritée de la tâche</label>
                 <select name="priority">
                     <option value="" disabled selected>Prioritée</option>
-                    <option value="low">Basse</option>
-                    <option value="medium">Moyenne</option>
-                    <option value="high">Haute</option>
+                    <option value="basse">Basse</option>
+                    <option value="moyenne">Moyenne</option>
+                    <option value="haute">Haute</option>
                 </select>
-
-                <input type="date" name="due_at">
+                
+                <label for="">Date d'échéance de la tâche</label>
+                <input type="date" name="due_date">
 
                 <input type="submit" value="Créer">
             </form>
